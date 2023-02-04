c88bbb139efc1df69adb2244d5134e7d28a33f70
Anakinツ <99360831+NicolasYakuzaBr@users.noreply.github.com>
2023-02-04 16:49:57 -0300

    Add files via upload

---
+++ Abrir_Links.py
@@ -0,0 +1,12 @@
+#pip install webbrowser
+
+import webbrowser
+print('Insira o link que você deseja abrir em seu navegador: ')
+webbrowser.open(url= input(''))
+print('Endereço encontrado! Abrindo o navegador....... ')
+
+#23/01/2023 https://www.youtube.com/watch?v=ToL2mON-z2c
+
+
+
+
